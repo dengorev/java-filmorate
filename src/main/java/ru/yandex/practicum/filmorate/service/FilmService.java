ca33ed36e5e8c4229dadb3e65edc4b4501917a34
Denis <dengorev361@gmail.com>
2024-10-04 23:45:01 +0300

    ТЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -7,11 +7,17 @@ import java.util.List;
 
 public interface FilmService {
     Film save(Film film);
+
     List<Film> getAll();
+
     Film update(Film film);
+
     Film getFilmById(int id);
+
     Film addLike(int id, int userId);
+
     Film deleteLike(int id, int userId);
+
     Collection<Film> getPopular(int count);
 }
 
